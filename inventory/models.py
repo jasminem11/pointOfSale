53505af4a626e3cc46a241a0a6d5743cfca2ed90
esejosue <josuegallardolara@hotmail.com>
2022-11-02 15:04:51 -0700

    11/2/2022 inventory model, starting admin for product inventory

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    product_name=models.CharField(max_length=200, unique=True);
+
+    def __str__(self):
+        return self.product_name
