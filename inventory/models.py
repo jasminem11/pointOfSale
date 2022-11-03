89aeb3a57acf09f1fdbf3a0f483838925278d57e
esejosue <josuegallardolara@hotmail.com>
2022-11-03 09:08:23 -0700

    Product model done, admin inventory good to go.

--- inventory/models.py
+++ inventory/models.py
@@ -2,7 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-    product_name=models.CharField(max_length=200, unique=True);
+    product_name=models.CharField(max_length=200, unique=True)
+    price=models.IntegerField(null=True)
+    image = models.ImageField(upload_to="photos/products", null=True)
+    stock = models.IntegerField(null=True)
+    is_available=models.BooleanField(default=True,null=True)
+    created_date=models.DateTimeField(auto_now_add=True)
+    modified_date=models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.product_name
