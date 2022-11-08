56ee3c5b6988043ae0adc1f56c9b0c689f1d271e
esejosue <josuegallardolara@hotmail.com>
2022-11-07 16:10:15 -0800

    11/7/2022 update, starting connection with front and back

--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+def inventory(request):
+    products=Product.objects.all().filter(is_available=True)
+
+    context={
+        "products":products,
+    }
+
+    return render(request, 'templates/home.html', context)
